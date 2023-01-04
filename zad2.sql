9ab8b71318add9294a04c836a157dc4fc71ad419
Szymon <113582467+szymon02913@users.noreply.github.com>
2023-01-04 17:37:55 +0100

    Update zad2.sql

--- zad2.sql
+++ zad2.sql
@@ -2,4 +2,9 @@ SELECT MIN(price) FROM products GROUP BY category_id;
 SELECT MAX(price) FROM products GROUP BY category_id;
 SELECT COUNT(category_id) FROM products
 SELECT COUNT(category_id) FROM customers
+SELECT COUNT(country) FROM customers;
+SELECT COUNT(id),AVG(price) FROM products WHERE category_id="1";
+SELECT SUM(price) FROM products WHERE category_id="2";
+SELECT MAX(price) FROM products WHERE category_id="1" OR category_id="2" GROUP BY category_id;
+
 
