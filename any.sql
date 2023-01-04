ec4bd9eda7a8cd3751188d9fc13d2b3c089b04ad
Szymon <113582467+szymon02913@users.noreply.github.com>
2023-01-04 17:44:41 +0100

    Update any.sql

--- any.sql
+++ any.sql
@@ -1,1 +1,4 @@
-ds
+SELECT * FROM products WHERE products.id = ANY(SELECT product_id FROM orders_details WHERE quantity = 10)
+SELECT * FROM products WHERE products.id = ANY(SELECT product_id FROM orders_details WHERE quantity > 99)
+SELECT * FROM products WHERE TRUE;
+SELECT * FROM products WHERE products.id = ALL (SELECT product_id FROM orders_details WHERE quantity = 10);
